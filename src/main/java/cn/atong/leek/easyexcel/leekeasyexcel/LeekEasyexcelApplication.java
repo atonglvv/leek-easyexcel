f02e5216b9f45736e9d32e01c66519982930788e
atongTs <atongLv@163.com>
2021-06-29 23:45:30 +0800

    add @MapperScan

--- src/main/java/cn/atong/leek/easyexcel/leekeasyexcel/LeekEasyexcelApplication.java
+++ src/main/java/cn/atong/leek/easyexcel/leekeasyexcel/LeekEasyexcelApplication.java
@@ -1,9 +1,11 @@
 package cn.atong.leek.easyexcel.leekeasyexcel;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.atong.leek.easyexcel.leekeasyexcel.mapper")
 public class LeekEasyexcelApplication {
 
     public static void main(String[] args) {
